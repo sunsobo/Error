e038cdcfe59c7447705aa3594094fbf6d60f21f8
sunsobolev <36673718+sunsobolev@users.noreply.github.com>
2023-09-07 10:50:47 +0300

    error

--- MyNews/NewsPaper/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NewsList, NewsDetail
-
-urlpatterns = [
-    path('', NewsList.as_view()),
-    path('<int:pk>', NewsDetail.as_view(), name='post_detail'),
-]
